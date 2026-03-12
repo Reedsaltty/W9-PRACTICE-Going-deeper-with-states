c9e199a5751b5ccaa622f1d98180d097bb9887b1
Reedsaltty <leaphourleumac@gmail.com>
2026-03-12 15:42:01 +0700

    commit

--- exercise 2/src/App.jsx
+++ exercise 2/src/App.jsx
@@ -21,13 +21,16 @@ const INITIAL_STUFFS = [
 export default function App() {
   const [stuffs, setStuffs] = React.useState(INITIAL_STUFFS);
 
+  const addItem = (newStuff) => {
+    setStuffs([...stuffs, newStuff]);
+  }
   return (
     <>
       <header>
         <h1>My Stuff</h1>
       </header>
 
-      <StuffForm></StuffForm>
+      <StuffForm Add={addItem} />
 
       <div className="stuff-list">
         {stuffs.map((stuff, index) => (
